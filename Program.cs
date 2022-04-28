5a048b81e5285e98528171756e70ec8542f59083
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2022-04-28 11:08:13 -0600

    Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSE210_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

6eeaf78d9436e03e0e5b074e172271b2a7428b35
Swilsonlds <91799590+Swilsonlds@users.noreply.github.com>
2022-04-30 20:30:26 -0600

    board methods complete

--- Program.cs
+++ Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CSE210_01
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {  
+             List<int> board = CreateBoard();
+             DisplayBoard(board);
         }
+        
+        static List<int> CreateBoard()
+        {
+            List<int> board = new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                board.Add(i + 1);
+            }
+            return board;
+        }
+
+         static void DisplayBoard(List<int> board)
+        {
+            Console.WriteLine();
+            Console.WriteLine($"{board[0]}|{board[1]}|{board[2]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[3]}|{board[4]}|{board[5]}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{board[6]}|{board[7]}|{board[8]}");
+            Console.WriteLine();  
+        }
+
+        
+        
+
     }
 }
